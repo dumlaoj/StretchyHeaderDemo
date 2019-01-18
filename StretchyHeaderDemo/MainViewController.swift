ad54cb29b61b52e0abad3d8c90a865d594a71e9f
Jordan <dumlaoj@hawaii.edu>
2019-01-18 11:29:00 -0800

    Init commit Remove MainStoryboard Create Main View Controller

--- StretchyHeaderDemo/MainViewController.swift
+++ StretchyHeaderDemo/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  StretchyHeaderDemo
 //
 //  Created by Jordan Dumlao on 1/18/19.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-
+  
+  
 }
-
